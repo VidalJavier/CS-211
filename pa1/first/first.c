d6244f403bbc518cbb299c984f6a22b867191a5d
Javier Vidal <vidaljavier1296@gmail.com>
2017-04-08 00:32:39 -0400

    Add files via upload

---
+++ pa1/first/first.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv){
+	if(argc < 2){
+		printf("error\n");
+		exit(0);
+	}
+	int i;
+	int val;
+	for(i = 1; i < argc; i++){
+		val = atoi(argv[i]);
+		int c;
+		for(c = 2; c < val; c++){
+			if(val % c == 0){
+				printf("no\n");
+				return 0;
+			}
+		}
+		printf("yes\n");
+	}
+	return 0;
+}
